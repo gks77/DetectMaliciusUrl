8bb827852ff488de2765c17ffa19fbe280557c13
gks77 <guddusharma027@gmail.com>
2018-04-09 01:49:52 +0530

    Download tweet from twitter

---
+++ getTweetFromTwitter.py
@@ -0,0 +1,32 @@
+''' 
+  Download tweet from twitter to get URL
+'''
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+import time
+
+access_token="2564921978-ulEYwcTKFfPhRFJdDFkL4jQpTWKdFPViGFKxnoK"
+access_token_secret="nYxnIYiBddLUYjKJ9zrUhpVwOFFEdQwbWiAfOS5t5I3hE"
+consumer_key="MW9NovQYcXVPVzM6FWaKRv7H7"
+consumer_secret="IXWq48EhicdosGjufs8hlv4AOxmPhHTs70YZLQSCVhyIo9g4HS"
+class StdoutListener(StreamListener):
+	def on_data(self,data):
+		try:
+			savefile= open("/home/guddu/Desktop/ML/tweetus.txt","a")
+			savefile.write(data)
+			#savefile.write('\n')
+			savefile.close()
+			return True
+		except BaseException (e):
+			print("Failed on data",str(e))
+			time.sleep(5)
+
+	def on_error(self,data):
+			print(status)
+
+l = StdoutListener()
+auth = OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+stream=Stream(auth,l)
+stream.filter(track=['us'])
